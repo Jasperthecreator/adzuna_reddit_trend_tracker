f3e25d813f06fb9bec2188b8aa160886b5e76e9d
SentientGoose <104914530+Jasperthecreator@users.noreply.github.com>
2025-06-02 23:26:26 +0530

    Update reddit.py
    updated secrets keys to the proper format

--- reddit.py
+++ reddit.py
@@ -3,16 +3,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv(dotenv_path='keys.env')
 reddit = praw.Reddit(
-    client_id=${{reddit_id}},
-    client_secret=${{reddit_secret}},
-    user_agent=${{reddit_u_agent}}
+    client_id=${{secrets.reddit_id}},
+    client_secret=${{secrets.reddit_secret}},
+    user_agent=${{secrets.reddit_u_agent}}
 )
 r_count = int(reddit.subreddit('datascience').active_user_count)
 print(r_count)
 
 import supabase
 from supabase import create_client, Client
-url = ${{supabase_url}}
-key = ${{supabase_key}}
+url = ${{secrets.supabase_url}}
+key = ${{secrets.supabase_key}}
 client = create_client(url, key)
 client.table('r_ds').insert({'highest_users': r_count}).execute()
