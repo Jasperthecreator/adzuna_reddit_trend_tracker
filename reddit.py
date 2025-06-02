3e367b90b4a622d9c445bed2e840812b3c2a8898
SentientGoose <jasperjanish@gmail.com>
2025-06-02 19:38:26 +0530

    added GH secrets

--- reddit.py
+++ reddit.py
@@ -3,16 +3,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv(dotenv_path='keys.env')
 reddit = praw.Reddit(
-    client_id=os.getenv('reddit_url'),
-    client_secret=os.getenv('reddit_key'),
-    user_agent=os.getenv('reddit_u_agent')
+    client_id=${{reddit_id}},
+    client_secret=${{reddit_secret}},
+    user_agent=${{reddit_u_agent}}
 )
 r_count = int(reddit.subreddit('datascience').active_user_count)
 print(r_count)
 
 import supabase
 from supabase import create_client, Client
-url = os.getenv('supabase_url')
-key = os.getenv('supabase_key')
+url = ${{supabase_url}}
+key = ${{supabase_key}}
 client = create_client(url, key)
 client.table('r_ds').insert({'highest_users': r_count}).execute()
